503d067c491b546177e46b4e6dfa22c9ca75bf77
Julian Kornberger <jk+github@digineo.de>
2013-01-24 22:16:00 +0100

    Added a 'var'

--- vendor/assets/javascripts/google_dfp.js
+++ vendor/assets/javascripts/google_dfp.js
@@ -23,13 +23,13 @@ $(function(){
     // define slots
     tags.each(function(){
       var $this = $(this);
-      googleAdSlot = googletag.defineSlot( $this.data('unit'), [$this.width(), $this.height()], this.id).addService(googletag.pubads());
+      var googleAdSlot = googletag.defineSlot( $this.data('unit'), [$this.width(), $this.height()], this.id).addService(googletag.pubads());
       if(typeof googletag.renderEndedCallback === "function") {
         googleAdSlot.oldRenderEnded = googleAdSlot.renderEnded;
         googleAdSlot.renderEnded = function() {
           googleAdSlot.oldRenderEnded();
           googletag.renderEndedCallback();
-	}
+        }
       }
     })
     
