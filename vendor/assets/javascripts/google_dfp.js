fa8d7d5b81cedbc322320802324e512588d63bce
Timo Göllner <timo.goellner@googlemail.com>
2016-09-16 16:23:48 +0200

    styling

--- vendor/assets/javascripts/google_dfp.js
+++ vendor/assets/javascripts/google_dfp.js
@@ -1,31 +1,31 @@
+/* global $ */
 $(function(){
-  
   var tags = $("div.google-dfp:visible");
-  
+
   if(tags.length == 0)
     return; // nothing to do
-  
+
   // initialize googletag-variable
   window.googletag = window.googletag || {};
   var googletag = window.googletag;
   googletag.cmd = [];
-  
+
   // Load script
   $.ajax({
     dataType: "script",
     cache: true,
-    url: '//www.googletagservices.com/tag/js/gpt.js'
+    url: "//www.googletagservices.com/tag/js/gpt.js"
   });
-  
+
   // async commands
   googletag.cmd.push(function() {
 
     tags.each(function(){
       var $this = $(this);
-      var unit  = $this.data('unit');
-      var size  = $this.data('size');
+      var unit  = $this.data("unit");
+      var size  = $this.data("size");
       var googleAdSlot = null;
-      
+
       // define Slot
       if(size){
         size = size.split(" ").map(function(v){ return v.split("x").map(function(w){ return parseInt(w) }) });
@@ -37,15 +37,15 @@ $(function(){
 
       // add Service
       googleAdSlot.addService(googletag.pubads());
-      
+
       // set Targeting
-      var targeting = $this.data('targeting');
+      var targeting = $this.data("targeting");
       if(targeting){
         $.each(targeting, function(k, v) {
           googleAdSlot.setTargeting(k, v);
         });
       }
-      
+
       if(typeof googletag.renderEndedCallback === "function") {
         googleAdSlot.oldRenderEnded = googleAdSlot.renderEnded;
         googleAdSlot.renderEnded = function() {
@@ -54,12 +54,12 @@ $(function(){
         }
       }
     })
-    
+
     // enable services
     googletag.pubads().enableSingleRequest();
     googletag.pubads().enableAsyncRendering();
     googletag.enableServices();
-    
+
     // display ads
     tags.each(function(){
       googletag.display(this.id);
@@ -69,5 +69,5 @@ $(function(){
       googletag.callback();
     }
   });
-  
+
 })
